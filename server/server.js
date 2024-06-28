54d465c7ad84d0a231c1a11373e035dbbe6559f7
Jashanpreet20 <jashankot20@gmail.com>
2024-06-28 15:55:05 +0530

    new changes

--- server/server.js
+++ server/server.js
@@ -33,30 +33,15 @@ app.use(cookieParser());
 
 app.use(cors({
         origin:"*",
+        methods:["POST","GET","PUT","DELETE"],
         credentials:true,
 }))
 
-app.use(function (req, res, next) {
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
 // connection to cloudinary
 
 cloudinary.cloudinaryConnect();
+
 app.use(fileUpload({ useTempFiles: true, tempFileDir: "/temp/" }));
 
 
@@ -71,10 +56,13 @@ app.get("/",(req,res) =>{
 })
  
 
-
-app.listen(PORT,() =>{
-    console.log(`server is running at ${PORT}`);
+app.listen(4000,() =>{
+    console.log(`server is running at 4000`);
 })
+
+// app.listen(4000,() =>{
+//     console.log(`server is running at ${PORT}`);
+// })
 
 // connected database
 dbConnect();
