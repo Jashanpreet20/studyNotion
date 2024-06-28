2972500a713ada8a977ddb10a12a8728ab31fc11
Jashanpreet20 <jashankot20@gmail.com>
2024-06-28 14:33:48 +0530

    spelling change

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ app.use(cookieParser());
 
 
 app.use(cors({
-        origin:"https://study-notion-six-bay.vercel.app/",
+        origin:"https://study-notion-six-bay.vercel.app",
         credentials:true,
 }))
 
