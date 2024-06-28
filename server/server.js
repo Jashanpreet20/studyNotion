6d8f0ec3ba470d468c3b82d64a23ff33930b17f8
Jashanpreet20 <jashankot20@gmail.com>
2024-06-28 16:29:14 +0530

    new change in port

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const cors=require('cors');
 const cookieParser=require('cookie-parser');
 
 const fileUpload=require('express-fileupload');
-const dotenv=require('dotenv');
+require('dotenv').config();
 
 const PORT = process.env.PORT || 4000;
 
@@ -60,7 +60,7 @@ app.listen(4000,() =>{
     console.log(`server is running at 4000`);
 })
 
-// app.listen(4000,() =>{
+// app.listen(PORT,() =>{
 //     console.log(`server is running at ${PORT}`);
 // })
 
